46097036e9ffd7d069f1213bdaf2ed7f71dc37f0
IvanPalacios2809 <ivanpalacios2809@gmail.com>
2020-11-28 02:14:36 -0800

    Terminando Integrales

--- Metodos/Trapecios.py
+++ Metodos/Trapecios.py
@@ -13,19 +13,36 @@ a=float(input("Dame el valor de a"))
 b=float(input("Dame el valor de b"))
 resultado_real=integrate(funcion, (x,a,b))
 h=(b-a)/n
-resultado=(h/2)*a + (h/2)*b
+resultado=(h/2)*a
+
+
+
+
+
+file=open("Integral.txt","a")
+error=resultado-resultado_real
+file.write("\niteracion: 0 h: "+str(h)+" Valor integral: "+str(resultado)+" error "+str(error))
+file.close()
+    
+    
+
 x=a+h
-iteracion=0
+iteracion=1
 while(x<=b-h):
     resultadofuncion=2*eval(funcion)
     resultado=resultado+(h/2)*(resultadofuncion)
     x=x+h
     file=open("Integral.txt","a")
     error=resultado-resultado_real
-    
     file.write("\niteracion: "+str(iteracion)+" h: "+str(h)+" Valor integral: "+str(resultado)+" error "+str(error))
     file.close()
     iteracion=iteracion+1
+file=open("Integral.txt","a")
+resultado=resultado + (h/2)*b
+error=resultado-resultado_real
+
+file.write("\niteracion: "+str(iteracion)+" h: "+str(h)+" Valor integral: "+str(resultado)+" error "+str(error))
+file.close()
 f=open("Integral.txt","r")
 print(f.read())    
 f.close()
